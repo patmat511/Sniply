8a071375c64aad06369ff7175482078201be5898
Patryk Matusiak <128796303+patmat511@users.noreply.github.com>
2026-04-04 12:24:45 +0200

    fix: Fix typo in schemas and add missing CORS

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["http://localhost", "http://localhost:80","http://localhost:4200"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- backend/schemas.py
+++ backend/schemas.py
@@ -7,7 +7,7 @@ class URLCreate(BaseModel):
 class URLResponse(BaseModel):
     id: int
     original_url: str
-    short_url: str
+    short_code: str
     clicks: int
     created_at: datetime
 
